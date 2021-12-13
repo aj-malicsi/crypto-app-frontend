f6905da2a62ab4171a2492360be636eec81c4ed4
Andre Malicsi <aj.malicsi@yahoo.com.ph>
2021-12-13 20:40:49 +0800

    adds working example for EMA chart

--- src/charts/EMAChart.jsx
+++ src/charts/EMAChart.jsx
@@ -15,7 +15,38 @@ import axios from 'axios'
 import {useEffect} from 'react'
 
 
+const key = 'KB26K4SV9OF3UUKK'
+var coin = "BTC"
+var fiat = "USD"
+var interval = "daily"
+var time_period =90
+var url = `https://www.alphavantage.co/query?function=EMA&symbol=${coin}${fiat}&interval=${interval}&time_period=${time_period}&series_type=open&apikey=${key}`;
 
+var emaArr = []
+var dateArr = []
+var headersList = {
+    'User-Agent': 'request'
+}
+
+axios.get(url, {headers: headersList,
+    
+}).then((response) =>{
+    var info = response.data["Technical Analysis: EMA"]
+    // console.log(response.data)
+
+    if (info.Note === undefined){
+        
+        for(const key of Object.keys(info)){
+          dateArr.unshift(key)
+          for(const emaKey of Object.keys(info[key])){ 
+            emaArr.unshift(info[key][emaKey])
+          }
+        }
+    }
+    console.log(emaArr)   
+    }
+    
+)
 
 
 
@@ -36,66 +67,46 @@ export const options = {
   plugins: {
     legend: {
       position: 'top',
+      display: false
     },
     title: {
       display: true,
-      text: 'Chart.js Line Chart',
+      text: 'EMA',
     },
+    label:{
+      display: false
+    }
   },
 };
 
-const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+// const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+const labels = dateArr
+
+
+
+
+
 
 export const data = {
   labels,
   datasets: [
     {
-      label: 'Dataset 1',
-      data: [1,2,3,4,5,100],
+      label: 'EMA',
+      // data: [1,2,3,4,5,100],
+      data: emaArr,
       borderColor: 'rgb(255, 99, 132)',
       backgroundColor: 'rgba(255, 99, 132, 0.5)',
     },
-    {
-      label: 'Dataset 2',
-      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-      borderColor: 'rgb(53, 162, 235)',
-      backgroundColor: 'rgba(53, 162, 235, 0.5)',
-    },
+    // {
+    //   label: 'Dataset 2',
+    //   data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+    //   borderColor: 'rgb(53, 162, 235)',
+    //   backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    // },
   ],
 };
 
 export function EMAChart() {
-    //api call
-    useEffect( () =>{
-        
-    const key = 'KB26K4SV9OF3UUKK'
-    var coin = "BTC"
-    var fiat = "USD"
-    var interval = "daily"
-    var time_period =7
-    var url = `https://www.alphavantage.co/query?function=EMA&symbol=${coin}${fiat}&interval=${interval}&time_period=${time_period}&series_type=open&apikey=${key}`;
-
-    var headersList = {
-        'User-Agent': 'request'
-        
-    }
-
-    axios.get(url, {headers: headersList,
-    
-    }).then((response) =>{
-        var info = response.data
-        // console.log(response.data)
-
-        if (info.Note === undefined){
-            console.log(info["Technical Analysis: EMA"]["2019-03-24"]["EMA"])
-            // var keys = info.keys(info["Technical Analysis: EMA"])
-        }
-                
-        }
-    )
-
-
-}, [])
-    
+     
   return <Line options={options} data={data} />;
 }
