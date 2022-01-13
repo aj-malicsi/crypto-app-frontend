d0ee16cc3406321112e6286df13b0e66941d5028
Andre Malicsi <aj.malicsi@yahoo.com.ph>
2022-01-13 21:01:08 +0800

    add functionality for trades list

--- src/display/TradesList.jsx
+++ src/display/TradesList.jsx
@@ -1,24 +1,48 @@
 import axios from 'axios'
+import { useEffect } from 'react'
+import { useState} from 'react'
+import { Link } from "react-router-dom"
+
 
 
 export default function TradesList(props){
+  
+    const [trades, setTrades] = useState([])
 
-    var url = "http://localhost:5000/trades"
-    var headersList = {
-        "Content-Type":"application/json"
-    }
-    axios({
-        method: 'GET',
-        url: url,
-        headers: headersList,
-      }).then((response) =>{
-        console.log("display response", response)
+    
+    useEffect(() =>{
+        var url = "http://localhost:5000/trades"
+        var headersList = {
+            "Content-Type":"application/json"
+        }
+        axios({
+            method: 'GET',
+            url: url,
+            headers: headersList,
+          }).then((response) =>{
+            console.log("display response", response)
+            setTrades(response.data)
+         
+    
+          })
 
-      })
-      
+
+    }, [])
+
+    console.log(trades)
+
+   
+
     return(
         <>
         trades list
+        {
+        trades.map(trade => 
+        <Link to={`/display-trade/${trade.id}`}> {trade.title} </Link>
+        
+        ) 
+        
+        }
         </>
     )
 
